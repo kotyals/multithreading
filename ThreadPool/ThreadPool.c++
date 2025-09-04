dfe92b5cb74cff0f3615c172dd5a86ac612257da
Shivagond Kotyal <shivagond2001@gmail.com>
2025-09-04 07:15:30 -0700

    separate out header and main files

--- ThreadPool/ThreadPool.c++
+++ ThreadPool/ThreadPool.c++
@@ -1,6 +1,42 @@
 #include"ThreadPool.h"
 
+ThreadPool::ThreadPool(size_t s): numThreads(s){
+    for(int i=0; i<numThreads; i++){
+        workers.emplace_back([this](){
+            //call woker here
+            this -> worker();
 
+        });
+    }
+}
+ThreadPool::~ThreadPool(){
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        stop = true;
+    }
+    cv.notify_all();
+    for(auto& worker : workers){
+        worker.join();
+    }
+}
+
+void ThreadPool::worker(){
+        while(true){
+        std::function<void()> task;
+            {
+                unique_lock<mutex> lock(queueMutex);
+                cv.wait(lock,[this]{ return stop || !tasks.empty(); });
+
+                if( stop && tasks.empty()) return;
+
+                task = std::move(tasks.front());
+                tasks.pop();
+
+            }
+            task();
+        }
+
+    }
 int square(int n){
     return n*n;
 }
@@ -9,8 +45,8 @@ int main(){
 
     auto f1 = pool.enqueue([](int n){
         return n*n;
-    },10);
-    auto f2 = pool.enqueue(square,20);
+    },10); //sending lambda
+    auto f2 = pool.enqueue(square,20); //sending function
     std::cout << "f1 result: " << f1.get() << std::endl;
     std::cout << "f2 result: " << f2.get() << std::endl;
     return 0;
--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -11,55 +11,20 @@ using namespace std;
 
 class ThreadPool {
 private:
-size_t numThreads;
+    size_t numThreads;
     bool stop = false;
     vector<thread> workers;
     queue<std::function<void()>> tasks;
     mutex queueMutex;
     condition_variable cv;
     
-    void worker(){
-        while(true){
-        std::function<void()> task;
-            {
-                unique_lock<mutex> lock(queueMutex);
-                cv.wait(lock,[this]{ return stop || !tasks.empty(); });
-
-                if( stop && tasks.empty()) return;
-
-                task = std::move(tasks.front());
-                tasks.pop();
-
-            }
-            task();
-        }
-
-    }
+    void worker();
     
 public:
-    ThreadPool(size_t s): numThreads(s){
-        for(int i=0; i<numThreads; i++){
-            workers.emplace_back([this](){
-                //call woker here   
-                this -> worker();
-
-            });
-        }
-    }
-    ~ThreadPool(){
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            stop = true;
-        }
-        cv.notify_all();
-        for(auto& worker : workers){
-            worker.join();
-        }
-
-
-    }
+    ThreadPool(size_t s);
+    ~ThreadPool();
+    
     template <typename T>
-
     std::future<int> enqueue(T&& t,int n){
         auto task = make_shared<packaged_task<int()>>(
             [func = forward<T>(t), n]() mutable{
